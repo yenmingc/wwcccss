4b022cda0e6c9bc3d448c69ca8fe1e7fe5adf73b
Yen-Ming Chen <ymc@MBPR-6B43.local>
2017-10-22 10:53:17 -0700

    Updated keyphrases API to use Azure Table and update entities with KeyPhrases.

--- KeyPhrases.py
+++ KeyPhrases.py
@@ -1,33 +1,37 @@
 import httplib, urllib, base64, json
 from openpyxl import Workbook
 from openpyxl import load_workbook
+from azure.storage.table import TableService, Entity
 
-# This program reads controls and use Azure Key phrases API to extract key phrases
-# Name of the file with cloud security standard
-cfname = 'NIST 800-53-controls.xlsx'
-# Name of the new file with keyphrases
-ofile = 'US_NIST_800-53-r4.xlsx'
-# File with subscription key in json format
+# This program reads controls from Azure Table and use Azure KeyPhrases API to extract key phrases
+# File with KeyPhrase API subscription key in json format
 keyfile = 'Credential.json'
 with open(keyfile,'r') as k:
     key = json.load(k)
 
-wb = load_workbook(filename = cfname)
-ws = wb['Sheet1']
+# File with storage account (Azure Table) info in json format
+keyfile = 'Table.json'
+with open(keyfile,'r') as k:
+    t = json.load(k)
+    acc = t['acc']
+    tkey = t['key']
+    tab = t['tab']
+
+table_service = TableService(account_name=acc, account_key=tkey)
+
+# KeyPhrases API headers
 headers = {
     'Content-Type': 'application/json',
     'Ocp-Apim-Subscription-Key': key['key'],
 }
 params = urllib.urlencode({
-
 })
 
-# Assume column B is id, F is text and I is for key phrases
-row = 1
-while not(ws['B'+str(row)].value is None and ws['F'+str(row)].value is None):
-    row = row + 1
-    id = ws['B'+str(row)].value
-    text = ws['F'+str(row)].value
+controls = table_service.query_entities('wwcccs', filter=None)
+
+for control in controls:
+    id = control.PartitionKey + '-' + control.RowKey
+    text = control.ControlDescription
     body = json.dumps({
         "documents" : [
             {
@@ -42,12 +46,10 @@ while not(ws['B'+str(row)].value is None and ws['F'+str(row)].value is None):
         response = conn.getresponse()
         data = json.loads(response.read())
         documents = data['documents']
-        print(str(row)+": "+documents[0]['id'])
-        ws['I'+str(row)].value = ', '.join(documents[0]['keyPhrases'])
-        #print(', '.join(documents[0]['keyPhrases']))
+        kp = ", ".join(documents[0]['keyPhrases'])
+        print(documents[0]['id']+": "+kp)
+        updated_control = {'PartitionKey': control.PartitionKey, 'RowKey': control.RowKey, 'KeyPhrases': kp}
+        table_service.insert_or_merge_entity('wwcccs', updated_control)
         conn.close()
     except Exception as e:
-        print(str(row)+": "+e.message+"\n"),response.status, response.reason, response.msg
-
-# Save final output file
-wb.save(ofile)
+        print(id+": "+e.message+"\n"),response.status, response.reason, response.msg
